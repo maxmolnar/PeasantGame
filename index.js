34985c6219d6c668337df69d2f8671aad094a6df
nmoln <molnarnick50@gmail.com>
2017-05-24 23:00:07 -0400

    made everything work
    thanks nick.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
+
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
+app.use(express.static(__dirname + '/js'));
 
 
 http.listen(3000, function () {
