4febc8c72a4419f7609c11558e81f706f71acbbe
nmoln <molnarnick50@gmail.com>
2017-05-25 09:42:04 -0400

    sockets stuff

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var functions = require('./js/Functions.js')();
+var io = require('socket.io')(http);
+
 
 //IDK if this works or not
 init();
@@ -14,6 +16,9 @@ app.get('/', function (req, res) {
 
 app.use(express.static(__dirname + '/js'));
 
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
 
 http.listen(3000, function () {
     console.log('listening on *:3000');
