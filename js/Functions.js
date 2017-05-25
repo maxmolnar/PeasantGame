2fc942267b307d2d43bfe262c9cee5c9ab569442
Max Molnar <molnarm@purdue.edu>
2017-05-25 10:22:55 -0400

    adding init implementation

--- js/Functions.js
+++ js/Functions.js
@@ -2,18 +2,26 @@
 
 //Array of tile objects - holds game state
 var board = [];
+var npcs = [];
+var players = [];
 
 //Theoretically allows calls from other files
 module.exports = function() {
 	//initializes game state at server start up
 	this.init = function() {
 		for (var i = 0; i < 50; i++) {
-			board[i] = {terrain: 'grass',
-						standing: 'peasant'};
+			var ter = 'grass';
+			var stand = 'empty';
+			if (i === 15 || i === 25 || i === 30) {
+				stand = 'peasant';
+				
+			}
+			board[i] = {terrain: ter,
+						standing: stand};
 		}
 	}
 
 	this.update = function() {
-		console.log("updating");
+		console.log("updating");	
 	}
 }
