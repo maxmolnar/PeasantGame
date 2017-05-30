3cc6e95f8fa5a37342cf6590dfc7ec88d038ae06
Max Molnar <molnarm@purdue.edu>
2017-05-30 10:53:31 -0400

    Major changes - added clusters
    Ask max

--- index.js
+++ index.js
@@ -4,12 +4,54 @@ var http = require('http').Server(app);
 var functions = require('./js/Functions.js')();
 io = require('socket.io')(http);
 var List = require("collections/list");
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 
 
+if (cluster.isMaster) {
+	//master handles all calculations
+  console.log(`Master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+
+  //Initilize game board
+  init();
+
+  //sets update to run every 5 seconds
+  setInterval(update, 5000);
+  console.log('Master updating board');
+
+} else {
+  //Workers handle all communication
+
+  app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/Index.html');
+  });
+
+  app.use(express.static(__dirname + '/js'));
+
+  io.on('connection', function(socket){
+    console.log('a user connected');
+    playerSpawn(); 
+  });
+
+  http.listen(3000);
+  console.log(`Worker ${process.pid} listening on *:3000`);
+}
+
+/*
 //IDK if this works or not
 init();
 //sets update to run every 5 seconds
 setInterval(update, 5000);
+console.log('Master updating board');
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/Index.html');
@@ -23,7 +65,8 @@ io.on('connection', function(socket){
     
 });
 
-http.listen(3000, function () {
-    console.log('listening on *:3000');
-});
+//http.listen(3000, function () {
+//    console.log('listening on *:3000');
+//});
+*/
 
